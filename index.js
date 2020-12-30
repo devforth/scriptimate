67ea0e21ec8509b593a9d0065481623e79fe1141
Ivan Borshchov <ivan@devforth.io>
2020-12-30 21:04:19 +0200

    progress

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const genHtml = () => {
         }
         body {
           transform-origin: top left;
-          transform: scale(1.2);
+          transform: scale(1.1);
         }
         </style>
       </head>
@@ -57,17 +57,28 @@ const genHtml = () => {
 String.prototype.replaceAll = function(target, replacement) {
   return this.split(target).join(replacement);
 };
+
+function firstDefined(...vals) {
+  for (let i = 0; i < vals.length; i += 1) {
+    if (vals[i] !== undefined) {
+      return vals[i];
+    }
+  }
+  return undefined;
+}
+
+
 const addPart = async (filename, left, top, opacity, scale) => {
   const f = await readFile(`src/${filename}.svg`, 'utf-8');
   parts[filename] ={
     filename,
     // todo remove ugly fix
     content: f.replaceAll('pattern0', `pattern${short.generate()}`).replaceAll('pattern1', `#pattern${short.generate()}`).replaceAll('pattern2', `#pattern${short.generate()}`).replaceAll('image0', `image${short.generate()}`).replaceAll('image1', `#image${short.generate()}`).replaceAll('image2', `#image${short.generate()}`),
-    top: +top || 0,
-    left: +left || 0,
-    opacity: +opacity||1,
+    top: +firstDefined(top, 0),
+    left: +firstDefined(left, 0),
+    opacity: +firstDefined(opacity, 1),
     index: Object.values(parts).length,
-    scale: +scale || 1.0,
+    scale: +firstDefined(scale, 1.0),
   };
   genHtml()
 }
@@ -76,23 +87,26 @@ const script = `
 place board 0 0
 place signin_board_task 17 149
 place transaction_list_board_task 17 253
-place app_backplate 361 383 0.2
-place app_signin_task 576 568 0.2
+place app_backplate 361 383 0.4
+place app_signin_task 576 568 0.4
+place app_transaction_list_task 852 568 0.4
 
-place app_transaction_list_task 852 568 0.2
 place highliter 576 568 0.0
 
 
 place cursor 321 323
 animate_400 move cursor 755 22
-animate_100 scale cursor 1.6
-animate_50 scale cursor 1
+
+animate_1500 pause
+animate_50 scale cursor 1.6
+animate_120 scale cursor 1
 animate_200 opacity app_backplate 1; opacity app_signin_task 1; oppacity app_transaction_list_task 1
-animate_500 move cursor 755 22
 animate_500 move cursor 755 568
 animate_200 opacity highliter 1; move cursor 739 701
-animate_100 scale cursor 1.6
-animate_50 scale cursor 1
+
+animate_150 pause
+animate_50 scale cursor 1.6
+animate_120 scale cursor 1
 `;
 
 // todo recover shadow at place - add class
@@ -102,7 +116,6 @@ animate_50 scale cursor 1
 
 (async () => {
   fsExtra.emptyDirSync(FRAMES_DIR);
-  log(`Foramt selected ${proc_args.format}, filename ${proc_args.filename}.${proc_args.format}`)
   const browser = await puppeteer.launch({args: ['--window-size=2320,1000'],});
   const page = await browser.newPage();
 
@@ -126,6 +139,9 @@ animate_50 scale cursor 1
     }
   }
   totalFrames = totalMs / 1.0e3 * FPS;
+  log(`Foramt selected: ${proc_args.format}
+Filename ${proc_args.filename}.${proc_args.format}
+Total duration: ${(totalMs / 1e3).toFixed(1)}s \n`);
 
   for (v of script.split('\n')) {
     const d1 = v.split(' ');
@@ -142,13 +158,13 @@ animate_50 scale cursor 1
       const ms = +cmd.replace('animate_', '');
       const freezer = {};
       const frames = ms / 1.0e3 * FPS;
-      for (i = 0; i < frames; i += 1) {
+      for (i = 1; i <= frames; i += 1) {
         await doFrame();
         for (let ags of argSets) {
-          const ags_arr = ags.split(' ');
+          const ags_arr = ags.trim().split(' ');
           const action = ags_arr[0];
-          const svg = ags_arr[1];
           if (action === 'move') {
+            const svg = ags_arr[1];
             const dstLeft = +ags_arr[2];
             const dstTop = +ags_arr[3];
             if (!freezer[svg]) {
@@ -157,12 +173,14 @@ animate_50 scale cursor 1
             parts[svg].top = freezer[svg].top + (dstTop - freezer[svg].top) * i / frames;
             parts[svg].left = freezer[svg].left + (dstLeft - freezer[svg].left) * i / frames;
           } else if (action === 'scale') {
+            const svg = ags_arr[1];
             const dstScale = +ags_arr[2];
             if (!freezer[svg]) {
               freezer[svg] = {scale: parts[svg].scale};
             }
             parts[svg].scale = freezer[svg].scale + (dstScale - freezer[svg].scale) * i / frames;
           } else if (action === 'opacity') {
+            const svg = ags_arr[1];
             const dstOpacity = +ags_arr[2];
             if (!freezer[svg]) {
               freezer[svg] = {opacity: parts[svg].opacity};

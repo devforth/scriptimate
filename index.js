30e261959ca9bbefeddc9dc7acd9aaf9b96f0545
Ivan Borshchov <ivan@devforth.io>
2020-12-30 20:36:09 +0200

    progress

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const util = require('util');
 const puppeteer = require('puppeteer');
 const short = require('short-uuid');
 const readFile = util.promisify(fs.readFile);
+const { spawn } = require('child_process');
+const fsExtra = require('fs-extra')
+const { ArgumentParser } = require('argparse');
+const { version } = require('./package.json');
+const { exception } = require('console');
+ 
+const log = console.log;
+const FPS = 25;
+const MAX_FILENAME_DIGS = 7;
+let cntr = 0;
+let totalFrames = 0;
+const FRAMES_DIR = 'frames';
+
+
+const parser = new ArgumentParser({
+  description: `Scriptimate v${version}`
+});
+ 
+parser.add_argument('-v', '--version', { action: 'version', version });
+parser.add_argument('-f', '--format', { help: 'format webm or mp4', default: 'mp4' });
+parser.add_argument('-fn', '--filename', { help: 'filename', default: 'out' });
+
+ 
+const proc_args = parser.parse_args();
 
 let parts = {};
 let html = '';
@@ -12,7 +36,7 @@ const genHtml = () => {
   const inner = Object.values(parts).sort((a,b) => {
       return a.index - b.index
   }).reduce((acc, p) => {
-    return `${acc}<div style="position:fixed;top:${p.top}px;left:${p.left}px;opacity:${p.opacity}">${p.content}</div>`
+    return `${acc}<div style="position:fixed;top:${p.top}px;left:${p.left}px;opacity:${p.opacity};transform:scale(${p.scale})">${p.content}</div>`
   }, '');
   html = `<html>
       <head>
@@ -23,7 +47,7 @@ const genHtml = () => {
         }
         body {
           transform-origin: top left;
-          transform: scale(2);
+          transform: scale(1.2);
         }
         </style>
       </head>
@@ -43,7 +67,7 @@ const addPart = async (filename, left, top, opacity, scale) => {
     left: +left || 0,
     opacity: +opacity||1,
     index: Object.values(parts).length,
-    scale: scale || 1.0,
+    scale: +scale || 1.0,
   };
   genHtml()
 }
@@ -53,34 +77,56 @@ place board 0 0
 place signin_board_task 17 149
 place transaction_list_board_task 17 253
 place app_backplate 361 383 0.2
+place app_signin_task 576 568 0.2
+
+place app_transaction_list_task 852 568 0.2
+place highliter 576 568 0.0
+
+
 place cursor 321 323
-animate_500 move cursor 755 22
-animate_30 scale cursor 1.4
+animate_400 move cursor 755 22
+animate_100 scale cursor 1.6
 animate_50 scale cursor 1
-animate_100 opacity app_backplate 1
+animate_200 opacity app_backplate 1; opacity app_signin_task 1; oppacity app_transaction_list_task 1
+animate_500 move cursor 755 22
+animate_500 move cursor 755 568
+animate_200 opacity highliter 1; move cursor 739 701
+animate_100 scale cursor 1.6
+animate_50 scale cursor 1
 `;
 
 // todo recover shadow at place - add class
 
-const log = console.log;
-const FPS = 25;
-let cntr = 0;
 
 
 
 (async () => {
+  fsExtra.emptyDirSync(FRAMES_DIR);
+  log(`Foramt selected ${proc_args.format}, filename ${proc_args.filename}.${proc_args.format}`)
   const browser = await puppeteer.launch({args: ['--window-size=2320,1000'],});
   const page = await browser.newPage();
 
   const doFrame = async () => {
     genHtml()
     await page.setContent(html);
-    await page.screenshot({path: `res/${(''+cntr).padStart(6, '0')}.png`});
+    await page.screenshot({path: `${FRAMES_DIR}/${(''+cntr).padStart(MAX_FILENAME_DIGS, '0')}.png`});
     cntr += 1;
+    log(`Frames gen: ${(cntr * 100.0 / (totalFrames + 1)).toFixed(2)}%`)
   }
 
   await page.setViewport({width: 2320, height: 0, deviceScaleFactor:2});
   await page._client.send('Emulation.clearDeviceMetricsOverride');
+
+  let totalMs = 0;
+  for (v of script.split('\n')) {
+    const d1 = v.split(' ');
+    const cmd = d1[0];
+    if (cmd.startsWith('animate_')) {
+      totalMs += +cmd.replace('animate_', '');
+    }
+  }
+  totalFrames = totalMs / 1.0e3 * FPS;
+
   for (v of script.split('\n')) {
     const d1 = v.split(' ');
     const cmd = d1[0];
@@ -111,13 +157,13 @@ let cntr = 0;
             parts[svg].top = freezer[svg].top + (dstTop - freezer[svg].top) * i / frames;
             parts[svg].left = freezer[svg].left + (dstLeft - freezer[svg].left) * i / frames;
           } else if (action === 'scale') {
-            const dstScale = +ags_arr[1];
+            const dstScale = +ags_arr[2];
             if (!freezer[svg]) {
               freezer[svg] = {scale: parts[svg].scale};
             }
             parts[svg].scale = freezer[svg].scale + (dstScale - freezer[svg].scale) * i / frames;
           } else if (action === 'opacity') {
-            const dstOpacity = +ags_arr[1];
+            const dstOpacity = +ags_arr[2];
             if (!freezer[svg]) {
               freezer[svg] = {opacity: parts[svg].opacity};
             }
@@ -134,11 +180,36 @@ let cntr = 0;
 
 
   await browser.close();
+  log('Frames generation done')
 
   fs.writeFile("index.html", html, function(err) {
     if(err) {
         return console.log(err);
     }
-    console.log("The file was saved!");
-}); 
+    log("The index.html file was saved!");
+  });
+
+  let ffmpeg_args = ['-framerate', `${FPS}/1`, '-i', `${FRAMES_DIR}/%0${MAX_FILENAME_DIGS}d.png`, '-r', ''+FPS, `${proc_args.filename}.${proc_args.format}`, '-y'];
+  if (proc_args.format === 'webm') {
+    ffmpeg_args = [...ffmpeg_args, '-c:v', 'libvpx-vp9', '-b:v', '2M']
+  } else if (proc_args.format === 'mp4') {
+    ffmpeg_args = [...ffmpeg_args, '-c:v', 'libx264',]
+  } else {
+    throw exception(`Unknown format: ${proc_args.format}`);
+  }
+  const ls = spawn('ffmpeg', ffmpeg_args);
+
+  ls.stdout.on('data', (data) => {
+    console.log(`ffmpeg: ${data}`);
+  });
+  
+  ls.stderr.on('data', (data) => {
+    console.error(`ffmpeg: ${data}`);
+  });
+  
+  ls.on('close', (code) => {
+    console.log(`ffmpeg exited with code ${code}`);
+  });
+  
+
 })();
--- readme.md
+++ readme.md
@@ -1,5 +1,20 @@
 
-ffmpeg -framerate 25/1 -i res/%06d.png -c:v libx264 -r 25 out.mp4 -y
 
 
-ffmpeg -framerate 25/1 -i res/%06d.png -c:v libvpx-vp9 -b:v 2M -r 25 out.webm -y
+MP4 used by default (faster compile, less optimized)
+
+npx run scriptimate
+
+To use webm:
+
+npx run scriptimate -f webm
+
+
+
+
+Under the hood next commands are used:
+
+ffmpeg -framerate 25/1 -i frames/%07d.png -c:v libx264 -r 25 out.mp4 -y
+
+
+ffmpeg -framerate 25/1 -i frames/%07d.png -c:v libvpx-vp9 -b:v 2M -r 25 out.webm -y

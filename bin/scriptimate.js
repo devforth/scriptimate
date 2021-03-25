96ddb79b382840911a56ddb1ae0f071d7383b6d1
Ivan Borshchov <ivan@devforth.io>
2021-03-25 23:32:30 +0200

    group progress

--- bin/scriptimate.js
+++ bin/scriptimate.js
@@ -42,9 +42,86 @@ FRAMES_DIR = proc_args.basedir + '/' + FRAMES_DIR;
 const parts = {};
 const timers = {};
 const boxholes = {};
+
+const groups = {};  // name => Array of lines
+const freezer = {}; 
+
 let pageScale = 1;
 let pageW;
 let pageH;
+let groupToAddNext = null;
+
+const ACTION_HANDLERS = {
+  move: (i, ags_arr, first_frame_in_animate, frames) => {
+    const svg = ags_arr[1];
+    if (!parts[svg]) {
+      log(`WARN: opacity not applied, part not found: ${svg}, line: \n${cmd}\n`);
+      return;
+    }
+    const dstLeft = ags_arr[2] === '-' ? parts[svg].left : eval(ags_arr[2]);
+    const dstTop =  ags_arr[3] === '-' ? parts[svg].top : eval(ags_arr[3]);
+    if (first_frame_in_animate) {
+      freezer[svg] = {...freezer[svg], top: parts[svg].top, left: parts[svg].left};
+    }
+    parts[svg].top = freezer[svg].top + (dstTop - freezer[svg].top) * i / frames;
+    parts[svg].left = freezer[svg].left + (dstLeft - freezer[svg].left) * i / frames;
+    // log('parts[svg].left', svg, dstLeft, parts[svg].left)
+  },
+  scale: (i, ags_arr, first_frame_in_animate, frames) => {
+    const svg = ags_arr[1];
+    if (!parts[svg]) {
+      log(`WARN: opacity not applied, part not found: ${svg}, line: \n${cmd}\n`);
+      return;
+    }
+    const dstScale = +ags_arr[2];
+    if (first_frame_in_animate) {
+      freezer[svg] = {...freezer[svg], scale: parts[svg].scale};
+    }
+    parts[svg].scale = freezer[svg].scale + (dstScale - freezer[svg].scale) * i / frames;
+  },
+  rotate: (i, ags_arr, first_frame_in_animate, frames) => {
+    const svg = ags_arr[1];
+    if (!parts[svg]) {
+      log(`WARN: rotate not applied, part not found: ${svg}, line: \n${cmd}\n`);
+      return;
+    }
+    const dstRotate = +ags_arr[2];
+    if (first_frame_in_animate) {
+      freezer[svg] = {...freezer[svg], rotate: parts[svg].rotate};
+    }
+    parts[svg].rotate = freezer[svg].rotate + (dstRotate - freezer[svg].rotate) * i / frames;
+  },
+  opacity: (i, ags_arr, first_frame_in_animate, frames) => {
+    const svg = ags_arr[1];
+    if (!parts[svg]) {
+      log(`WARN: opacity not applied, part not found: ${svg}, line: \n${cmd}\n`);
+      return;
+    }
+    const dstOpacity = +ags_arr[2];
+    if (first_frame_in_animate) {
+      freezer[svg] = {...freezer[svg], opacity: parts[svg].opacity};
+    }
+    parts[svg].opacity = freezer[svg].opacity + (dstOpacity - freezer[svg].opacity) * i / frames;
+  },
+  resize_div: (i, ags_arr, first_frame_in_animate, frames) => {
+    const svg = ags_arr[1];
+    if (!parts[svg]) {
+      log(`WARN: resize_div not applied, part not found: ${svg}, line: \n${cmd}\n`);
+      return;
+    }
+    if (parts[svg].type !== 'block') {
+      log(`WARN: resize_div could be applied only to type block: not ${parts[svg].type}, part: ${svg}, line: \n${cmd}\n`);
+      return;
+    }
+    const dstW = ags_arr[2] === '-' ? parts[svg].w : +ags_arr[2];
+    const dstH =  ags_arr[3] === '-' ? parts[svg].h : +ags_arr[3];
+    if (first_frame_in_animate) {
+      freezer[svg] = {...freezer[svg], w: parts[svg].w, h: parts[svg].h};
+    }
+    parts[svg].h = freezer[svg].h + (dstH - freezer[svg].h) * i / frames;
+    parts[svg].w = freezer[svg].w + (dstW - freezer[svg].w) * i / frames;
+  }
+}
 
 const genHtml = () => {
   const inner = Object.values(parts).sort((a,b) => {
@@ -98,8 +175,8 @@ function firstDefined(...vals) {
 
 let add_part_counter = 0;
 
-const addPart = async (filename, left, top, opacity, scale, toBoxHole) => {
-  const f = await fs.readFile(`${proc_args.basedir}/src/${filename}.svg`, 'utf-8');
+const addPart = (filename, left, top, opacity, scale, toBoxHole) => {
+  const f = fsExtra.readFileSync(`${proc_args.basedir}/src/${filename}.svg`, 'utf-8').toString();
   const partIds = {};
   let withUniquifiedIDs = f.replace(/id="(.*?)"/g, (_, v) => {
     if (!partIds[v]) {
@@ -123,9 +200,10 @@ const addPart = async (filename, left, top, opacity, scale, toBoxHole) => {
     extrastyle: '',
     toBoxHole,
   };
+  freezer[filename] = {};
 };
 
-const addDiv = async (name, left, top, w, h, opacity, c, ...rest) => {
+const addDiv = (name, left, top, w, h, opacity, c, ...rest) => {
   //+rest.join(' ').replaceAll('"', '').replaceAll("'", '')
   const content = eval(c);
   parts[name] = {
@@ -142,7 +220,7 @@ const addDiv = async (name, left, top, w, h, opacity, c, ...rest) => {
   }
 }
 
-const addBoxHole = async (name, left, top, w, h) => {
+const addBoxHole = (name, left, top, w, h) => {
   boxholes[name] = {
     name,
     top: +firstDefined(eval(top), 0),
@@ -166,7 +244,7 @@ const setPseudoInterval = (f, ms) => {
   return o;
 }
 
-const addStyle = async (part, style) => {
+const addStyle = (part, style) => {
   if (!parts[part]) {
     log(`WARN: style not applied, part not found: ${part}`)
     return
@@ -174,7 +252,7 @@ const addStyle = async (part, style) => {
   parts[part].extrastyle = style;
 }
 
-const schedule_eval = async (name, ms, ...rest) => {
+const schedule_eval = (name, ms, ...rest) => {
   const code = rest.join(' ');
   // todo check that timer already scheduled and drop warn
   timers[name] = setPseudoInterval(
@@ -196,7 +274,7 @@ const schedule_eval = async (name, ms, ...rest) => {
   )
 }
 
-const unschedule = async (name) => {
+const unschedule = (name) => {
   // chack that schedulled and drop warn
   delete timers[name];
 }
@@ -250,10 +328,27 @@ let skipFrames = 0;
 
   fsExtra.emptyDirSync(FRAMES_DIR);
 
-  for (v of script.split('\n')) {
-    const d1 = v.split(' ');
-    const cmd = d1[0];
-    let argSets = d1.slice(1).join(' ');
+
+  for(const  [file_line, line] of script.split('\n').entries() ) {
+    if (!line.trim()) {
+      // empty line
+      continue;
+    }
+    if (line.startsWith(' ') || line.startsWith('\t')) {
+      if (groupToAddNext) {
+        groups[groupToAddNext] = v.trim()
+        continue;
+      } else {
+        throw `Line can start with whitespace only if it follows after define_group, on line ${file_line}\m${line}`;
+      }
+    } else {
+      groupToAddNext = null;
+    }
+
+    const line_splitted_by_whitespace = line.split(' ');
+    const cmd = line_splitted_by_whitespace[0];
+
+    let argSets = line_splitted_by_whitespace.slice(1).join(' ');
     
     if (argSets) {
       argSets = argSets.split('&&');
@@ -281,110 +376,50 @@ let skipFrames = 0;
         let v = s.split('=')
         global[v[0]] = eval(v[1])
       })
-    }else if (cmd === 'place') {
+    } else if (cmd === 'place') {
       let args = argSets[0].split(' ');
-      await addPart(...args);
+      addPart(...args);
     }
     else if (cmd === 'place_div') {
       let args = argSets[0].split(' ');
-      await addDiv(...args);
+      addDiv(...args);
     }
     else if (cmd === 'place_boxhole') {
       let args = argSets[0].split(' ');
-      await addBoxHole(...args);
+      addBoxHole(...args);
     }
     else if (cmd === 'schedule_eval') {
       let args = argSets[0].split(' ');
-      await schedule_eval(...args);
+      schedule_eval(...args);
     }
     else if (cmd === 'unschedule') {
       let args = argSets[0].split(' ');
-      await unschedule(...args);
+      unschedule(...args);
     }
-    
     else if (cmd === 'addstyle') {
-      let args =  argSets[0].split(' ');
-      await addStyle(args[0], args.slice(1).join(' '));
+      let args = argSets[0].split(' ');
+      addStyle(args[0], args.slice(1).join(' '));
+    }
+    else if (cmd.startsWith('define_group')) {
+      let grpName = argSets[0].trim();
+      if (grpName.endsWith(':')) {
+        grpName = grpName.slice(0, -1)
+      }
+      groupToAddNext = grpName;
     }
     else if (cmd.startsWith('animate_')) {
-      const ms = +cmd.replace('animate_', '');
-      const freezer = {};
-      const frames = Math.round(ms / 1.0e3 * FPS);
-      for (i = 1; i <= frames; i += 1) {
+      const duration_ms = +cmd.replace('animate_', '');
+      
+      const frames = Math.round(duration_ms / 1.0e3 * FPS);
+      for (let i = 1; i <= frames; i += 1) {
+        const first_frame_in_animate = i === 1;
         Object.values(timers).forEach((t) => t.tick(1000.0 / FPS));
         await doFrame();
         for (let ags of argSets) {
           const ags_arr = ags.trim().split(' ');
           const action = ags_arr[0];
-          if (action === 'move') {
-            const svg = ags_arr[1];
-            if (!parts[svg]) {
-              log(`WARN: opacity not applied, part not found: ${svg}, line: \n${cmd}\n`);
-              continue;
-            }
-            const dstLeft = ags_arr[2] === '-' ? parts[svg].left : eval(ags_arr[2]);
-            const dstTop =  ags_arr[3] === '-' ? parts[svg].top : eval(ags_arr[3]);
-            if (!freezer[svg]) {
-              freezer[svg] = {top: parts[svg].top, left: parts[svg].left};
-            }
-            parts[svg].top = freezer[svg].top + (dstTop - freezer[svg].top) * i / frames;
-            parts[svg].left = freezer[svg].left + (dstLeft - freezer[svg].left) * i / frames;
-            // log('parts[svg].left', svg, dstLeft, parts[svg].left)
-          } else if (action === 'scale') {
-            const svg = ags_arr[1];
-            if (!parts[svg]) {
-              log(`WARN: opacity not applied, part not found: ${svg}, line: \n${cmd}\n`);
-              continue;
-            }
-            const dstScale = +ags_arr[2];
-            if (!freezer[svg]) {
-              freezer[svg] = {scale: parts[svg].scale};
-            }
-            parts[svg].scale = freezer[svg].scale + (dstScale - freezer[svg].scale) * i / frames;
-          } else if (action === 'rotate') {
-            const svg = ags_arr[1];
-            if (!parts[svg]) {
-              log(`WARN: rotate not applied, part not found: ${svg}, line: \n${cmd}\n`);
-              continue;
-            }
-            const dstRotate = +ags_arr[2];
-            if (!freezer[svg]) {
-              freezer[svg] = {rotate: parts[svg].rotate};
-            }
-            parts[svg].rotate = freezer[svg].rotate + (dstRotate - freezer[svg].rotate) * i / frames;
-          } else if (action === 'opacity') {
-            const svg = ags_arr[1];
-            if (!parts[svg]) {
-              log(`WARN: opacity not applied, part not found: ${svg}, line: \n${cmd}\n`);
-              continue;
-            }
-            const dstOpacity = +ags_arr[2];
-            if (!freezer[svg]) {
-              freezer[svg] = {opacity: parts[svg].opacity};
-            }
-            parts[svg].opacity = freezer[svg].opacity + (dstOpacity - freezer[svg].opacity) * i / frames;
-          } else if (action === 'resize_div') {
-            const svg = ags_arr[1];
-            if (!parts[svg]) {
-              log(`WARN: resize_div not applied, part not found: ${svg}, line: \n${cmd}\n`);
-              continue;
-            }
-            if (parts[svg].type !== 'block') {
-              log(`WARN: resize_div could be applied only to type block: not ${parts[svg].type}, part: ${svg}, line: \n${cmd}\n`);
-              continue;
-            }
-            const dstW = ags_arr[2] === '-' ? parts[svg].w : +ags_arr[2];
-            const dstH =  ags_arr[3] === '-' ? parts[svg].h : +ags_arr[3];
-            if (!freezer[svg]) {
-              freezer[svg] = {w: parts[svg].w, h: parts[svg].h};
-            }
-            parts[svg].h = freezer[svg].h + (dstH - freezer[svg].h) * i / frames;
-            parts[svg].w = freezer[svg].w + (dstW - freezer[svg].w) * i / frames;
-          }
-
-          
+          ACTION_HANDLERS[action](i, ags_arr, first_frame_in_animate, frames)
         }
-
       }
     }
   }

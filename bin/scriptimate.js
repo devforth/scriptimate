06ad64b2404bb93b52d5b7e46bbe6f285db4b937
Ivan Borshchov <ivan@devforth.io>
2022-02-22 17:15:13 +0200

    remove google cloud font

--- bin/scriptimate.js
+++ bin/scriptimate.js
@@ -245,8 +245,6 @@ const genHtml = (allParts) => {
   }, '');
   return `<html>
       <head>
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
         <style>
         * {
           -webkit-font-smoothing:antialiased;
